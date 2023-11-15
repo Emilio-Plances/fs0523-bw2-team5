3348324004809b98133ed361a50ae7f4412739c9
Emilio-Plances <emilio.plances@gmail.com>
2023-11-15 17:29:26 +0100

    Template home, search fatti!

--- Progetto BW/js/prova-api.js
+++ Progetto BW/js/prova-api.js
@@ -1,18 +1,7 @@
 import {SearchTrack} from "./class/SearchTrack.js";
-
+import {HomeCard} from "./class/HomeCard.js";
 const API=`https://striveschool-api.herokuapp.com/api/deezer/`;
-
-let invio= document.querySelector(`#search-button`);
-
-invio.addEventListener(`click`,(e)=>{
-   e.preventDefault();
-   let input= document.querySelector(`#search-bar`);
-   
-   getSearch(input.value);
-   input.value=``;
-})
-
-const IDRANDOM=[{
+const ALGORITMO=[{
    id:92,
    type:`artist`
 },{
@@ -80,6 +69,23 @@ const IDRANDOM=[{
    type:`track`
 }
 ]; //questi dati dovrebbero arrivare dall'algoritmo del backend 
+let centralBox=document.querySelector(`#central-box`);
+let homeButton=document.querySelector(`#home-button`);
+let input= document.querySelector(`#search-bar`);
+
+getHome();
+
+homeButton.addEventListener(`click`,()=>{
+   resetContainer(centralBox);
+   getHome();
+})
+
+input.addEventListener(`keydown`,(e)=>{
+   if (e.key === "Enter"){
+      getSearch(input.value);
+      input.value=``;
+   }
+})
 
 function getSearch(query){
    query=query.replace(" ", "");
@@ -92,7 +98,6 @@ function getSearch(query){
    })
    .then(res=>res.json())
    .then(risultatiRicerca=>{
-      console.log(risultatiRicerca);
       let boxTrack=document.querySelector(`#central-box`);
       resetContainer(boxTrack);
 
@@ -108,22 +113,57 @@ function getSearch(query){
    })
 }
 
-function getHome(id) {
-   
+function getHome() {
+   getHomeTemplate();
+   console.dir(centralBox);
+   ALGORITMO.forEach(element=>{
+      fetch(`${API}${element.type}/${element.id}`,{
+         "method": "GET",
+         "headers": {
+            "Content-Type": "application/json"
+         }
+      })
+    .then(res=>res.json())
+    .then(dato=>{
+      console.log(dato);
+      let name;let artist;let immagine;let container; let artistID;
+      let song;let albumID;
+      if(element.type==`artist`){
+         name=dato.name;
+         immagine=dato.picture_medium;
+         artist=`Artist`;
+         artistID=dato.id;
+         container=document.querySelector(`#artist-home-container`);
+      }
+      if(element.type==`album`){
+         name=dato.title;
+         albumID=dato.id;
+         artist=dato.artist.name;
+         artistID=dato.artist.id;
+         immagine=dato.cover_medium;
+         container=document.querySelector(`#album-home-container`);
+      }
+      if(element.type==`track`){
+         name=dato.title;
+         artist=dato.artist.name;
+         artistID=dato.artist.id;
+         immagine=dato.album.cover_medium;
+         song=dato.preview;
+         container=document.querySelector(`#track-home-container`);
+      }
+      new HomeCard(API, name, artist, immagine, container, artistID, song, albumID);
+    })
+   }) 
 }
+
+function getHomeTemplate(){
+   let temp= document.querySelector(`#home-template`);
+   let clone= temp.content.cloneNode(true);
+   centralBox.append(clone);
+};
 
 function resetContainer(container){
    while(container.firstChild){
       container.removeChild(container.firstChild);
    }
 }
-
-        // if(element.type==`album`){
-         //    let albumTitle=element.album.title;
-         //    
-         //    console.log(albumTitle, artistName, albumCover);
-         // }
-         // if(element.type==`artist`){
-         //    let artistImg=element.artist.picture_medium;
-         //    console.log(artistName,artistImg)
-         // }
